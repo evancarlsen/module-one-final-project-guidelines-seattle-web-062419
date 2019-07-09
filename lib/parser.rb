21916636161b3253337966e76ac2cda0ce4aff95
evancarlsen <evan@evancarlsen.com>
2019-07-09 16:12:12 -0700

    parser creates array of camera names

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,7 +5,8 @@ class Parser
     def self.start_parsing
         rover_array = get_array
         rover_names = get_rover_names(rover_array)
-        puts rover_names
+        camera_names = get_camera_names(rover_array)
+        puts camera_names
 
     end
 
@@ -21,5 +22,19 @@ class Parser
         names = rover_array.map do |rover_hash|
             rover_hash["name"]
         end
+    end
+
+    def self.cameras_from_rover(rover_hash)
+        cameras = rover_hash["cameras"].map do |camera_hash|
+            camera_hash["name"]
+        end
+    end
+
+    def self.get_camera_names(rover_array)
+        camera_names = []
+        rover_array.each do |rover_hash|
+            camera_names << cameras_from_rover(rover_hash)
+        end
+        camera_names.flatten.uniq!
     end
 end
